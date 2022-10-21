d3e6daaa2732c9ebf908813e46d8f389dcff4620
crovelli <chiara.rovelli@cern.ch>
2022-10-21 10:57:31 +0200

    Datasets updated

--- Ntuplizer/crab.py
+++ Ntuplizer/crab.py
@@ -1,4 +1,4 @@
-from CRABClient.UserUtilities import config #, getUsernameFromSiteDB
+from CRABClient.UserUtilities import config 
 config = config()
 
 config.General.requestName = ''
@@ -6,17 +6,14 @@ config.General.transferLogs = True
 config.General.transferOutputs = True
 
 config.JobType.pluginName = 'Analysis'
-#config.JobType.psetName = 'nanoanalyzercrab_cfg_22_SingleMu.py'
 config.JobType.psetName = 'nanoanalyzercrab_cfg_22_DoubleEle.py'
 config.JobType.maxMemoryMB = 4000
 config.JobType.numCores = 1
 
 config.Data.inputDataset = ''
-#config.Data.inputDBS = 'phys03'
 config.Data.inputDBS = 'global'
 config.Data.splitting = 'FileBased'
 config.Data.unitsPerJob = 2
-#config.Data.totalUnits = 400
 
 #config.Data.outLFNDirBase = '/store/user/jodedra/Run3/' #% (getUsernameFromSiteDB())
 #config.Data.publication = True
@@ -27,68 +24,40 @@ config.Data.unitsPerJob = 2
 #config.Site.blacklist = ['T2_US_Purdue']
 
 
-#config.General.workArea = 'crab_singlemu/crab_singlemumisseddataset'
-config.General.workArea = 'crab_doubleele/crab_doubleelemisseddataset'
+config.General.workArea = 'crab_doubleele_new2/crab_doubleele_new2_Fv1'
 
-#name = 'EphemeralZeroBias8'
-#name = 'ZeroBias10'
+#name='/EGamma/Run2022C-PromptReco-v1/MINIAOD/DoubleEleTest'
+#config.General.requestName = name.replace('/','_')
+#config.Data.inputDataset = '/EGamma/Run2022C-PromptReco-v1/MINIAOD'
+#config.Data.outputDatasetTag = 'Run2022C-PromptReco-v1_DoubleEleTest'
+#config.Data.outLFNDirBase = '/store/user/crovelli/EGamma2022/%s' % (config.General.workArea)
 
-#name='/ParkingSingleMuon2/Run2022D-PromptReco-v2/MINIAODBPeak'
-#config.Data.outLFNDirBase = '/store/user/jodedra/Run3SingleMunotrigmatch/20220831/'
-#config.Data.inputDataset = '/ParkingSingleMuon2/Run2022D-PromptReco-v2/MINIAOD'
+#name='/EGamma/Run2022D-PromptReco-v1/MINIAOD/DoubleEleTest'
+#config.General.requestName = name.replace('/','_')
+#config.Data.inputDataset = '/EGamma/Run2022D-PromptReco-v1/MINIAOD'
+#config.Data.outputDatasetTag = 'Run2022D-PromptReco-v1_DoubleEleTest'
+#config.Data.outLFNDirBase = '/store/user/crovelli/EGamma2022/%s' % (config.General.workArea)
 
-name='/EGamma/Run2022C-PromptReco-v1/MINIAOD/DoubleEleTest'
+#name='/EGamma/Run2022D-PromptReco-v2/MINIAOD/DoubleEleTest'
+#config.General.requestName = name.replace('/','_')
+#config.Data.inputDataset = '/EGamma/Run2022D-PromptReco-v2/MINIAOD'
+#config.Data.outputDatasetTag = 'Run2022D-PromptReco-v2_DoubleEleTest'
+#config.Data.outLFNDirBase = '/store/user/crovelli/EGamma2022/%s' % (config.General.workArea)
+
+#name='/EGamma/Run2022E-PromptReco-v1/MINIAOD/DoubleEleTest'
+#config.General.requestName = name.replace('/','_')
+#config.Data.inputDataset = '/EGamma/Run2022E-PromptReco-v1/MINIAOD'
+#config.Data.outputDatasetTag = 'Run2022E-PromptReco-v1_DoubleEleTest'
+#config.Data.outLFNDirBase = '/store/user/crovelli/EGamma2022/%s' % (config.General.workArea)
+
+name='/EGamma/Run2022F-PromptReco-v1/MINIAOD/DoubleEleTest'
 config.General.requestName = name.replace('/','_')
-config.Data.inputDataset = '/EGamma/Run2022C-PromptReco-v1/MINIAOD'
-config.Data.outputDatasetTag = 'Run2022C-PromptReco-v1_DoubleEleTest'
-config.Data.outLFNDirBase = '/store/user/cquarant/EGamma2022/%s' % (config.General.workArea)
+config.Data.inputDataset = '/EGamma/Run2022F-PromptReco-v1/MINIAOD'
+config.Data.outputDatasetTag = 'Run2022F-PromptReco-v1_DoubleEleTest'
+config.Data.outLFNDirBase = '/store/user/crovelli/EGamma2022/%s' % (config.General.workArea)
 
-# name='/EGamma/Run2022D-PromptReco-v1/MINIAOD/DoubleEleTest'
-# config.General.requestName = name.replace('/','_')
-# config.Data.inputDataset = '/EGamma/Run2022D-PromptReco-v1/MINIAOD'
-# config.Data.outputDatasetTag = 'Run2022D-PromptReco-v1_DoubleEleTest'
-# config.Data.outLFNDirBase = '/store/user/cquarant/EGamma2022/%s' % (config.General.workArea)
-
-# name='/EGamma/Run2022D-PromptReco-v2/MINIAOD/DoubleEleTest'
-# config.General.requestName = name.replace('/','_')
-# config.Data.inputDataset = '/EGamma/Run2022D-PromptReco-v2/MINIAOD'
-# config.Data.outputDatasetTag = 'Run2022D-PromptReco-v2_DoubleEleTest'
-# config.Data.outLFNDirBase = '/store/user/cquarant/EGamma2022/%s' % (config.General.workArea)
 
 config.section_('User')
 config.section_('Site')
 config.Site.storageSite = 'T2_IT_Rome'
-
-#config.Data.inputDataset = '/' + name + '/ytakahas-winter21-30e4efebaefe52740b9ca928c2409cd7/USER'
-
-
-#for a in [1,2,3,4,5,6,7]:
-    
-#
-#'/EphemeralZeroBias1/Run2018D-PromptReco-v2/MINIAOD'
-#'/EphemeralZeroBias2/Run2018D-PromptReco-v2/MINIAOD'
-#'/EphemeralZeroBias3/Run2018D-PromptReco-v2/MINIAOD'
-#'/EphemeralZeroBias4/Run2018D-PromptReco-v2/MINIAOD'
-#'/EphemeralZeroBias5/Run2018D-PromptReco-v2/MINIAOD'
-#'/EphemeralZeroBias6/Run2018D-PromptReco-v2/MINIAOD'
-#'/EphemeralZeroBias7/Run2018D-PromptReco-v2/MINIAOD'
-#'/EphemeralZeroBias8/Run2018D-PromptReco-v2/MINIAOD'
-#
-#'/EphemeralZeroBias1/Run2018D-v1/RAW'
-#'/EphemeralZeroBias2/Run2018D-v1/RAW'
-#'/EphemeralZeroBias3/Run2018D-v1/RAW'
-#'/EphemeralZeroBias4/Run2018D-v1/RAW'
-#'/EphemeralZeroBias5/Run2018D-v1/RAW'
-#'/EphemeralZeroBias6/Run2018D-v1/RAW'
-#'/EphemeralZeroBias7/Run2018D-v1/RAW'
-#'/EphemeralZeroBias8/Run2018D-v1/RAW'
-
-
-
-
-#config.General.requestName = 'EphemeralZeroBias3'
-#config.Data.inputDataset = '/EphemeralZeroBias3/Run2018D-PromptReco-v2/MINIAOD'
-#config.Data.secondaryInputDataset = '/EphemeralZeroBias3/Run2018D-v1/RAW'
-
-
 
