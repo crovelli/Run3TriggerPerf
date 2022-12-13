c36b29919d1513183099bd5ac6be4c8104da1deb
crovelli <chiara.rovelli@cern.ch>
2022-12-13 11:05:44 +0100

    new collections added

--- Ntuplizer/nanoanalyzercrab_cfg_22_DoubleEle.py
+++ Ntuplizer/nanoanalyzercrab_cfg_22_DoubleEle.py
@@ -15,47 +15,10 @@ from FWCore.ParameterSet.VarParsing import VarParsing
 process = cms.Process("Nano")
 process.load("FWCore.MessageLogger.MessageLogger_cfi")
 process.load("TrackingTools/TransientTrack/TransientTrackBuilder_cfi")
-#process.load("Configuration.Geometry.GeometryIdeal_cff") # for 2011
-#process.load("Configuration.StandardSequences.Geometry_cff") # for 2010
 process.load("Configuration.StandardSequences.MagneticField_cff")
 process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
 process.load("Configuration.Geometry.GeometryRecoDB_cff")
 process.load('Configuration.StandardSequences.EndOfProcess_cff')
-
-################################### VARPASSING ###################################
-# Use VarParsing to specify your input directory and output file
-# Comment Varparsing if you want to submit job using CRAB
-# because CRAB does not support input directory as we put input dataset directly
-#options = VarParsing('analysis')
-
-
-#options.register(
-#    "inputDir",
-#    "",
-#    VarParsing.multiplicity.singleton,
-#    VarParsing.varType.string,
-#    "Input directory with inputs"
-#    )
-#options.register(
-#    "outputName",
-#    "",
-#    VarParsing.multiplicity.singleton,
-#    VarParsing.varType.string,
-#    "Output name"
-#    )
-#options.parseArguments()
-
-#if (options.inputDir == ""):
-#    sys.exit("Directory to find input file where???")
-#else:
-    # 2010 VM
-    #InputDir = "/home/cms-opendata/CMSSW_4_2_8/NanoAOD/NanoAnalyzer/" + options.inputDir
-    # 2011 NAF/HTC
-    # InputDir = "/nfs/dust/cms/user/zulaiha/PhD/CMSSW_5_3_32/src/NanoAOD/NanoAnalyzer/" + options.inputDir
-    # 2015 NAF/HTC
-    #InputDir = "/nfs/dust/cms/user/zulaiha/PhD/CMSSW_7_6_1/src/NanoAOD/NanoAnalyzer/" + options.inputDir
-
-##################################################################################
 
 ###################################### GLOBAL TAG ################################
 # Change the global tag accordingly
@@ -74,7 +37,6 @@ process.GlobalTag.globaltag = '123X_dataRun3_Prompt_v12'
 # Intialize MessageLogger and output report
 process.load("FWCore.MessageLogger.MessageLogger_cfi")
 process.MessageLogger.cerr.threshold = 'INFO'
-#process.MessageLogger.categories.append('Nano')
 process.MessageLogger.cerr.FwkReport.reportEvery = 10000
 process.options   = cms.untracked.PSet(wantSummary = cms.untracked.bool(True))
                                         
@@ -108,16 +70,6 @@ process.load("JetMETCorrections.Configuration.JetCorrectionServicesAllAlgos_cff"
 # To test locally or submit batch job using condor, use this:
 #fileinPut = FileUtils.loadListFromFile (InputDir)
 
-#process.source = cms.Source("PoolSource",
-#                            fileNames = cms.untracked.vstring(*fileinPut)
-#)
-
-# To submit batch job using CRAB or test locally (2nd option), use this:
-
-#files = FileUtils.loadListFromFile("/eos/user/j/jodedra/BPARKINGNANOSTUFF/CMSSW_12_4_0_pre3/src/PhysicsTools/BParkingNano/test/singlefileboffifical.txt")
-#files.extend(FileUtils.loadListFromFile("/eos/user/j/jodedra/BPARKINGNANOSTUFF/CMSSW_12_4_0_pre3/src/PhysicsTools/BParkingNano/test/singlefileboffifical.txt"))
-
-
 inputFiles= [
     '/store/data/Run2022D/EGamma/MINIAOD/PromptReco-v2/000/357/899/00000/990a1665-d685-4623-8ddb-803c01962243.root',
 ]
@@ -125,9 +77,7 @@ inputFiles= [
 process.source = cms.Source("PoolSource",
 # for crab
 	  fileNames = cms.untracked.vstring (inputFiles),
-                            #fileNames = cms.untracked.vstring(*files)
 )
-#print(options.inputFiles)
 
 ##################################################################################
 
@@ -149,19 +99,5 @@ process.nano_ = cms.EDAnalyzer('NanoAnalyzer',
                               HLT = cms.InputTag("TriggerResults","","HLT"),
                               triggerobjects = cms.InputTag("slimmedPatTrigger"),
                               l1EG = cms.InputTag("caloStage2Digis", "EGamma"), 
-                              # Change this:
-                              # If HTC/VM:
-                              #outFile = cms.string(options.outputName),
-                              # If interactive:
-                              #outFile = cms.string('test.root'), 
-                              # If CRAB:
-                              # make sure the name is same as the crab config
-#                              outFile = cms.string('22ZeroBias136.root'),
-
-                              # Change this:
-                              # If MC:
-                              #isData = cms.bool(False)
-                              # If Data:
-#                              isData = cms.bool(True)
 )
 process.p = cms.Path(process.nano_)
